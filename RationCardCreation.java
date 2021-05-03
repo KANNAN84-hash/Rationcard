d95d6f150ae53f98be4ae1735b3f71cadc209c9f
KANNAN84-hash <82588986+KANNAN84-hash@users.noreply.github.com>
2021-05-03 15:23:21 +0530

    Add files via upload

---
+++ RationCardCreation.java
@@ -0,0 +1,21 @@
+package rationcard;
+
+public class RationCardCreation {
+	public String name;
+	public int age;
+	public String address;
+	public String gender;
+	public long phNo;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "RationCardCreation [name=" + name + ", age=" + age + ", address=" + address + ", gender=" + gender
+				+ ", phNo=" + phNo + "]";
+	}
+	
+	
+	
+
+}
